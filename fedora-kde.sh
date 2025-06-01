8656c05742b86ea7bd16f1534f5d0839a1cbf75e
ChickenIQ <36515619+ChickenIQ@users.noreply.github.com>
2025-06-01 18:23:54 +0300

    Update fedora-kde.sh

--- fedora-kde.sh
+++ fedora-kde.sh
@@ -1,9 +1,9 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-delete fedora
-dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf config-manager setopt fedora-cisco-openh264.enabled=1
-dnf update @core
-dnf install rpmfusion-\*-appstream-data
-dnf group remove desktop-accesibility kde-apps kde-media kde-pim libreoffice admin-tools
-dnf autoremove
-dnf update
+dnf update @core -y
+dnf install rpmfusion-\*-appstream-data -y
+dnf group remove desktop-accesibility kde-apps kde-media kde-pim libreoffice admin-tools -y
+dnf autoremove -y
+dnf update -y
